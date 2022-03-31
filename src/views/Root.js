0d30441d102059760ce623e43a0e74dc5068cfe5
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2022-03-31 07:52:21 +0200

    git init

---
+++ src/views/Root.js
@@ -0,0 +1,12 @@
+import './Root.css';
+import UseReducer from '../components/UseReducer';
+
+function Root() {
+  return (
+    <div className="App">
+      <UseReducer />
+    </div>
+  );
+}
+
+export default Root;
