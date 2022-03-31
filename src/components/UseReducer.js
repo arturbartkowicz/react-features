5944cd3cd94721a600945aa2afd90f7191aad175
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2022-03-31 08:04:30 +0200

    added reducer function

--- src/components/UseReducer.js
+++ src/components/UseReducer.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+
+const initialState = { count: 0 };
+
+// The reducer function
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    case 'reset':
+      return { count: (state.count = 0) };
+    default:
+      return { count: state.count };
+  }
+}
 
 const UseReducer = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <>
-      <div>Siemka</div>
+      Count: {state.count}
+      <br />
+      <br />
+      <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
     </>
   );
 };
